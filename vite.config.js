d7c0273a6f7c3a116b3546614500660e08393f86
Luxe Deploy <deploy@local>
2025-12-17 14:45:26 -0300

    refactor: Remove base property from Vite config for HashRouter

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // O nome do repositório é usado como caminho base para o deploy no GitHub Pages
-  base: '/testebarbearia/',
   plugins: [react()],
   resolve: {
     alias: {
