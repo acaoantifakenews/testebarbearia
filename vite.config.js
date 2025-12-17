b1c6726cc62c6d7f13359312b70625a8409e56fa
Luxe Deploy <deploy@local>
2025-12-17 14:26:27 -0300

    fix: Adjust vite config for GH Pages and use react-swc plugin

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // O nome do repositório é usado como caminho base para o deploy no GitHub Pages
+  base: '/luxe-barber-suite-main/',
+  plugins: [react()],
+})
