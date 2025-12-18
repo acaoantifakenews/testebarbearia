8eff1e1c21b85d519c66f3ee23a5373c54b32a90
Luxe Deploy <deploy@local>
2025-12-18 00:22:36 -0300

    fix(app): wrap app in StrictMode to resolve render error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AppProviders } from "./components/AppProviders.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <AppProviders>
-    <App />
-  </AppProviders>,
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>,
 );
